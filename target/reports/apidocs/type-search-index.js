533de254fe7de60fe0be46733ab2b3977566b6c0
Jorgen Lindstrom <jorgen.lindstrom@ductus.se>
2026-05-06 10:55:18 +0200

    build(FKPOC-635): bump rimfrost-process-asyncapi to 1.1.0

--- target/reports/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"se.fk.rimfrost.framework.process","l":"ProcessService"},{"p":"se.fk.rimfrost.framework.process","l":"RegelService"}];updateSearchResults();
