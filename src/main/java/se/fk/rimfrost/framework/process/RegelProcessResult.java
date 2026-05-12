55ec888a4156170a7d7e362a62828adcd52cf610
Jorgen Lindstrom <jorgen.lindstrom@ductus.se>
2026-05-12 14:05:41 +0200

    fix/FKPOC-770: extract felkod and felmeddelande to avoid mutable object exposure

--- src/main/java/se/fk/rimfrost/framework/process/RegelProcessResult.java
+++ src/main/java/se/fk/rimfrost/framework/process/RegelProcessResult.java
@@ -1,23 +1,30 @@
 package se.fk.rimfrost.framework.process;
 
 import se.fk.rimfrost.framework.regel.RegelErrorInformation;
+import se.fk.rimfrost.framework.regel.RegelFelkod;
 import se.fk.rimfrost.framework.regel.Utfall;
 
 public class RegelProcessResult {
 
     private final Utfall utfall;
-    private final RegelErrorInformation error;
+    private final RegelFelkod felkod;
+    private final String felmeddelande;
 
     public RegelProcessResult(Utfall utfall, RegelErrorInformation error) {
         this.utfall = utfall;
-        this.error = error;
+        this.felkod = error != null ? error.getFelkod() : null;
+        this.felmeddelande = error != null ? error.getFelmeddelande() : null;
     }
 
     public Utfall getUtfall() {
         return utfall;
     }
 
-    public RegelErrorInformation getError() {
-        return error;
+    public RegelFelkod getFelkod() {
+        return felkod;
+    }
+
+    public String getFelmeddelande() {
+        return felmeddelande;
     }
 }
