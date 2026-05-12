b013ef65a3e50ea2d26f5c1ef17e5e3b3bd82b81
Jorgen Lindstrom <jorgen.lindstrom@ductus.se>
2026-05-12 14:13:56 +0200

    Fix spotless error

--- src/main/java/se/fk/rimfrost/framework/process/RegelProcessResult.java
+++ src/main/java/se/fk/rimfrost/framework/process/RegelProcessResult.java
@@ -4,27 +4,32 @@ import se.fk.rimfrost.framework.regel.RegelErrorInformation;
 import se.fk.rimfrost.framework.regel.RegelFelkod;
 import se.fk.rimfrost.framework.regel.Utfall;
 
-public class RegelProcessResult {
+public class RegelProcessResult
+{
 
-    private final Utfall utfall;
-    private final RegelFelkod felkod;
-    private final String felmeddelande;
+   private final Utfall utfall;
+   private final RegelFelkod felkod;
+   private final String felmeddelande;
 
-    public RegelProcessResult(Utfall utfall, RegelErrorInformation error) {
-        this.utfall = utfall;
-        this.felkod = error != null ? error.getFelkod() : null;
-        this.felmeddelande = error != null ? error.getFelmeddelande() : null;
-    }
+   public RegelProcessResult(Utfall utfall, RegelErrorInformation error)
+   {
+      this.utfall = utfall;
+      this.felkod = error != null ? error.getFelkod() : null;
+      this.felmeddelande = error != null ? error.getFelmeddelande() : null;
+   }
 
-    public Utfall getUtfall() {
-        return utfall;
-    }
+   public Utfall getUtfall()
+   {
+      return utfall;
+   }
 
-    public RegelFelkod getFelkod() {
-        return felkod;
-    }
+   public RegelFelkod getFelkod()
+   {
+      return felkod;
+   }
 
-    public String getFelmeddelande() {
-        return felmeddelande;
-    }
+   public String getFelmeddelande()
+   {
+      return felmeddelande;
+   }
 }
